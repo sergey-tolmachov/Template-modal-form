ddb4981656c1e55d680f5304f16716aeb7dff576
Sergey Tolmachov <s.tolmachov@icloud.com>
2016-02-14 00:50:34 +0200

    First Upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// $(window).scroll(function() {
+//     if ($(document).scrollTop() > 300) {
+// 	   	$(".header-container2").fadeIn();
+// 	   	}
+// 	   	else {
+// 	    $(".header-container2").fadeOut();
+//       }
+// });
